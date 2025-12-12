4615002333c40aedd0ed68757965a74ca75a530d
Charles Severance <csev@umich.edu>
2025-12-12 09:31:03 -0500

    First cut as the schema

--- database.php
+++ database.php
@@ -9,6 +9,7 @@ if ( !isset($PDOX) ) {
 
 // Dropping tables
 $DATABASE_UNINSTALL = array(
+"drop table if exists {$CFG->dbprefix}aipaper_like",
 "drop table if exists {$CFG->dbprefix}aipaper_comment",
 "drop table if exists {$CFG->dbprefix}aipaper_result"
 );
@@ -21,6 +22,8 @@ array( "{$CFG->dbprefix}aipaper_result",
     raw_submission TEXT NULL,
     ai_enhanced_submission TEXT NULL,
     student_comment TEXT NULL,
+    flagged      BOOLEAN NOT NULL DEFAULT FALSE,
+    flagged_by   INTEGER NULL,  -- user_id who flagged this submission
     json         TEXT NULL,
 
     updated_at  TIMESTAMP NULL,
@@ -30,6 +33,11 @@ array( "{$CFG->dbprefix}aipaper_result",
         FOREIGN KEY (`result_id`)
         REFERENCES `{$CFG->dbprefix}lti_result` (`result_id`)
         ON DELETE CASCADE ON UPDATE CASCADE,
+
+    CONSTRAINT `{$CFG->dbprefix}aipaper_result_ibfk_2`
+        FOREIGN KEY (`flagged_by`)
+        REFERENCES `{$CFG->dbprefix}lti_user` (`user_id`)
+        ON DELETE SET NULL ON UPDATE CASCADE,
 
     PRIMARY KEY(result_id)
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8"),
@@ -65,6 +73,29 @@ array( "{$CFG->dbprefix}aipaper_comment",
 
     INDEX idx_result_id (result_id),
     INDEX idx_user_id (user_id)
+) ENGINE = InnoDB DEFAULT CHARSET=utf8"),
+
+array( "{$CFG->dbprefix}aipaper_like",
+"create table {$CFG->dbprefix}aipaper_like (
+    like_id      INTEGER NOT NULL KEY AUTO_INCREMENT,
+    comment_id   INTEGER NOT NULL,
+    user_id      INTEGER NOT NULL,
+
+    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT `{$CFG->dbprefix}aipaper_like_ibfk_1`
+        FOREIGN KEY (`comment_id`)
+        REFERENCES `{$CFG->dbprefix}aipaper_comment` (`comment_id`)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+
+    CONSTRAINT `{$CFG->dbprefix}aipaper_like_ibfk_2`
+        FOREIGN KEY (`user_id`)
+        REFERENCES `{$CFG->dbprefix}lti_user` (`user_id`)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+
+    UNIQUE(comment_id, user_id),
+    INDEX idx_comment_id (comment_id),
+    INDEX idx_user_id (user_id)
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8")
 
 );
@@ -74,12 +105,12 @@ $DATABASE_UPGRADE = function($oldversion) {
     global $CFG, $PDOX;
 
     // Initial version
-    if ( $oldversion < 202401010000 ) {
+    if ( $oldversion < 202512120000 ) {
         // Tables created in initial install
-        return 202401010000;
+        return 202512120000;
     }
 
-    return 202401010000;
+    return 202512120000;
 }; // Don't forget the semicolon on anonymous functions :)
 
 // Do the actual migration if we are not in admin/upgrade.php
